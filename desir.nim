9cb86f55e44d8514eaf64f747751d90ba528fca2
Abdelkader ALLAM <abdelkader.allam@gmail.com>
2016-01-29 19:16:24 +0000

    forgot to commit

--- desir.nim
+++ desir.nim
@@ -139,15 +139,14 @@ macro readCommandsFile(commandsFile: string): stmt =
 
 readCommandsFile("commands.csv")
 
-  
 
-
-var r = open()
-echo "keys: " & $r.keys("*")
-echo "set a 1" & $r.set("a",1)
-echo "get a" & $r.get("a")
-echo "incr b" & $r.incr("b")
-echo "get f" & $r.get("f")
-echo "get f f: " & $r.get("f","f")
+#var r = open()
+#echo "ping: " & $r.ping()
+#echo "keys: " & $r.keys("*")
+#echo "set a 1" & $r.set("a",1)
+#echo "get a" & $r.get("a")
+#echo "incr b" & $r.incr("b")
+#echo "get f" & $r.get("f")
+#echo "get f f: " & $r.get("f","f")
 
 
--- test.nim
+++ test.nim
@@ -6,4 +6,8 @@ echo "set a 1" & $r.set("a",1)
 echo "get a" & $r.get("a")
 echo "incr b" & $r.incr("b")
 echo "get f" & $r.get("f")
-echo "get f f: " & $r.get("f","f")
+#echo "get f f: " & $r.get("f","f")
+
+for i in 1..1_000_000:
+  discard r.set("a",i)
+  
